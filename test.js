b8f3cf45b24b185298836c889fe87fb6f580fdbb
Samuel David Gonsalves <sgonsal@ncsu.edu>
2017-11-07 14:14:40 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+/*
 var assert = require('assert');
  describe('Array', function() {
    describe('#indexOf()', function() {
@@ -6,3 +7,4 @@ var assert = require('assert');
      });
    });
  }); 
+*/
