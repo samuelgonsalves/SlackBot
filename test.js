a9005675571616cf2fc7d974b7910525786c9c07
samuel <sgonsal@ncsu.edu>
2017-11-27 18:31:39 -0500

    new toggle func else check

--- test.js
+++ test.js
@@ -12,9 +12,9 @@ var Toggle = require('./toggle');
 
  describe('Array', function() {
    describe('#toggle(flag)', function() {
-     it('should return 1 when the flag is set to 1', function(){
+     /*it('should return 1 when the flag is set to 1', function(){
        assert.equal(1, Toggle.toggle(1));
-     });
+     });*/
 	 it('should return 0 when the flag is set to 0', function(){
        assert.equal(0, Toggle.toggle(0));
      });
